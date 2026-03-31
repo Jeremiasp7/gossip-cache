c6ebf999ac8a83e0b75df56d6bf74636c26c7d30
Jeremias <jeremiasp2004@gmail.com>
2026-03-30 23:32:59 -0300

    adding the observer pattern interfaces

--- src/main/java/br/com/core/model/NodeInfo.java
+++ src/main/java/br/com/core/model/NodeInfo.java
@@ -7,14 +7,16 @@ public class NodeInfo {
     private UUID id; // the node identifier
     private String address; // hostname of the node
     private int port; // the port where the node is
+    private long lastHeartbeat; // the last heartbeat of the node
 
     // class constructors
     public NodeInfo() {}
 
-    public NodeInfo(UUID id, String address, int port) {
+    public NodeInfo(UUID id, String address, int port, long lastHeartbeat) {
         this.id = id;
         this.address = address;
         this.port = port;
+        this.lastHeartbeat = lastHeartbeat;
     }
 
     //getters and setters
@@ -40,6 +42,14 @@ public class NodeInfo {
 
     public void setPort(int port) {
         this.port = port;
+    }
+
+    public long getLastHeartbeat() {
+        return lastHeartbeat;
+    }
+
+    public void setLastHeartbeat(long lastHeartbeat) {
+        this.lastHeartbeat = lastHeartbeat;
     }
     
 }
---
+++ src/main/java/br/com/core/patterns/EventType.java
@@ -0,0 +1,6 @@
+package patterns;
+
+public enum EventType {
+    
+    NODE_JOINED, NODE_FAILED
+}
